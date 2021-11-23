c4e357f5deb46c889bf09055e6920d5688c280bc
Cielo <76010662@ucsm.edu.pe>
2021-11-23 12:11:18 -0500

    Finalización Experiencia N°2

---
+++ InicioSesionGoogle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InicioSesionGoogle.Startup))]
+namespace InicioSesionGoogle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
