11782b5767cdbf14bdcf1e30b02c1d153b9fa0ea
fabianportillodc <72370904@ucsm.edu.pe>
2021-11-24 05:40:05 -0500

    Ejercicio Propuesto Registro

---
+++ AuthApp/Cookies.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookies.aspx.cs" Inherits="AuthApp.Cookies" %>
+
+<!DOCTYPE html>   
+<html xmlns="http://www.w3.org/1999/xhtml">   
+<head runat="server">   
+    <title></title>   
+</head>   
+<body>   
+    <form id="form1" runat="server">   
+        <div>   
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>   
+        </div>   
+    </form>   
+</body>   
+</html>
---
+++ AuthApp/Cookies.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace AuthApp
+{
+    public partial class Cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //-------------- Creando Cookie --------------------------//   
+            // Creando una instancia de HttpCookie con el nombre especifico de "estudiante"
+            HttpCookie cokie = new HttpCookie("estudiante");
+            // Asignando un valor para el cookie  creado 
+            cokie.Value = "Juan Peréz";
+            // Agregando Cookie a la instancia response   
+            Response.Cookies.Add(cokie);
+            //--------------- Obteniendo cookie -------------------------//   
+            var co_val = Response.Cookies["estudiante"].Value;
+            Label1.Text = co_val;
+        }
+    }
+}
